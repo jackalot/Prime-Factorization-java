d212a9f8347fb6c0ce4d1a9563834155d8884f42
jackalot <68657425+jackalot@users.noreply.github.com>
2023-11-27 11:35:32 -0500

    StartNumber now stores an input dialog
    we now store an input dialog from JOption pane that uses our message

--- src/Start.java
+++ src/Start.java
@@ -3,6 +3,7 @@ public class Start {
 	public static void main(String[] args)
 	{
 		String message = "Please enter a number";
-		int startNumber;
+		int startNumber = Integer.parseInt(JOptionPane.showInputDialog(message));
+		
 	}
 }
